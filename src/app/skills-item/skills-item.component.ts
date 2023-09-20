0d7fee21b14e85d379d1e3c48d3e812bbddff439
Tomasz Jąder <tomasz.jader@gmail.com>
2023-09-20 23:31:05 +0200

    finish skills-item component

---
+++ src/app/skills-item/skills-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-item',
+  templateUrl: './skills-item.component.html',
+  styleUrls: ['./skills-item.component.scss']
+})
+export class SkillsItemComponent {
+
+}
