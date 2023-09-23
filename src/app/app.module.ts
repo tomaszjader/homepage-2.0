a9757c3f6c4b41193406821c132e8f8d6821dacc
Tomasz Jąder <tomasz.jader@gmail.com>
2023-09-23 08:56:51 +0200

    add skills-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SkillsItemComponent } from './skills-item/skills-item.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsItemComponent
+    SkillsItemComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skills-section/skills-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-section',
+  templateUrl: './skills-section.component.html',
+  styleUrls: ['./skills-section.component.scss']
+})
+export class SkillsSectionComponent {
+  skillsArray: string[] = ['— Semantic HTML5', '— Semantic HTML5',
+  '— Semantic HTML5', '— Semantic HTML5', '— Semantic HTML5', '— Semantic HTML5',
+  '— Semantic HTML5'];
+}
