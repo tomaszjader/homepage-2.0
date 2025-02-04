0073f63a5f7a5c34127a85caa80cb2338d7ac472
Tomasz Jąder <tomasz.jader@gmail.com>
2025-02-04 23:37:54 +0100

    remove console log

--- src/app/projects-section/projects-section.component.ts
+++ src/app/projects-section/projects-section.component.ts
@@ -12,7 +12,6 @@ export class ProjectsSectionComponent {
   constructor(private githubapiService:GithubapiService){
     this.githubapiService.getData().subscribe((data: Project[]) => {
       if (data) {
-        // console.log(data);
         this.data = data.filter(
           (repo: Project) => repo.description && repo.homepage
         );
