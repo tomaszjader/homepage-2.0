8ffdd7627a5d8cd346d2e3fe8a63574b1be8e5c6
Tomasz Jąder <tomasz.jader@gmail.com>
2026-01-08 22:08:09 +0100

    add post search

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -8,11 +8,24 @@ import { BlogService, BlogPost } from '../services/blog.service';
 })
 export class BlogListComponent implements OnInit {
   posts: BlogPost[] = [];
+  filteredPosts: BlogPost[] = [];
+  searchText: string = '';
 
   constructor(private blogService: BlogService) {}
 
   ngOnInit(): void {
     window.scrollTo(0, 0); // Ensure we start at the top
     this.posts = this.blogService.getPosts();
+    this.filteredPosts = this.posts;
+  }
+
+  filterPosts(): void {
+    if (!this.searchText) {
+      this.filteredPosts = this.posts;
+    } else {
+      this.filteredPosts = this.posts.filter(post =>
+        post.title.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    }
   }
 }
