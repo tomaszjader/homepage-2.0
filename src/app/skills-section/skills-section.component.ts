d577366c05a42f2a52802eb649fdb9b3413e41d4
Tomasz Jąder <tomasz.jader@gmail.com>
2026-01-10 23:22:14 +0100

    udate info

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -10,8 +10,8 @@ export class SkillsSectionComponent {
   '— Markdown', '— Figma', '— CSS grid', '— Forms',
   '— SASS', '— TypeScript','— JavaScript ES6 + features','— Git','— Working with API','— Angular'];
   knowSomethingAboutArray: string[] = ['— React', '— Ruby',
-    '— Flask', '— Figma', '— make.com', '— LLM',
-    '— OpenAI API'];
+    '— Flask', '— Figma', '— n8n', '— LLM',
+    '— OpenAI API', '— Gemini API'];
     wantToLearnArray: string[] = ['— Node JS', '— Electron',
       '— Server side rendering', '— Gatsby', '— Websockets'];
 }
