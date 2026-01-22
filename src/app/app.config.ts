83f019a99c573f890bd0c60a7614da5bee1d448a
Tomasz Jąder <tomasz.jader@gmail.com>
2026-01-22 22:12:12 +0100

    removal of the service worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -25,10 +25,6 @@ export const appConfig: ApplicationConfig = {
                     deps: [HttpClient]
                 },
                 defaultLanguage: 'pl'
-            }),
-            ServiceWorkerModule.register('ngsw-worker.js', {
-                enabled: !isDevMode(),
-                registrationStrategy: 'registerWhenStable:30000'
             })
         )
     ]
